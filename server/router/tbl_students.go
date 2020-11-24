f2740ea5868e6e2b0763f38ae938f166ad1f1674
崔　正偉 <zhengwei.cui@dhc.com.cn>
2020-11-24 21:01:04 +0800

    增加学生信息管理

--- server/router/tbl_students.go
+++ server/router/tbl_students.go
@@ -9,11 +9,11 @@ import (
 func InitTblStudentsRouter(Router *gin.RouterGroup) {
 	TblStudentsRouter := Router.Group("students").Use(middleware.JWTAuth()).Use(middleware.CasbinHandler()).Use(middleware.OperationRecord())
 	{
-		TblStudentsRouter.POST("createTblStudents", v1.CreateTblStudents)   // 新建TblStudents
-		TblStudentsRouter.DELETE("deleteTblStudents", v1.DeleteTblStudents) // 删除TblStudents
+		TblStudentsRouter.POST("createTblStudents", v1.CreateTblStudents)             // 新建TblStudents
+		TblStudentsRouter.DELETE("deleteTblStudents", v1.DeleteTblStudents)           // 删除TblStudents
 		TblStudentsRouter.DELETE("deleteTblStudentsByIds", v1.DeleteTblStudentsByIds) // 批量删除TblStudents
-		TblStudentsRouter.PUT("updateTblStudents", v1.UpdateTblStudents)    // 更新TblStudents
-		TblStudentsRouter.GET("findTblStudents", v1.FindTblStudents)        // 根据ID获取TblStudents
-		TblStudentsRouter.GET("getTblStudentsList", v1.GetTblStudentsList)  // 获取TblStudents列表
+		TblStudentsRouter.PUT("updateTblStudents", v1.UpdateTblStudents)              // 更新TblStudents
+		TblStudentsRouter.GET("findTblStudents", v1.FindTblStudents)                  // 根据ID获取TblStudents
+		TblStudentsRouter.GET("getTblStudentsList", v1.GetTblStudentsList)            // 获取TblStudents列表
 	}
 }
